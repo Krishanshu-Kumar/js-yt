e8d44bd6a6c8a67574ac7d8cfa4a967c48e68626
Krishanshu-Kumar <106949856+Krishanshu-Kumar@users.noreply.github.com>
2025-02-11 11:40:28 +0000

    2nd Commit done

---
+++ Basics/datatype.js
@@ -0,0 +1,9 @@
+let str = "Hello";  // String
+let num = 42;       // Number
+let big = 9007199254740991n; // BigInt
+let bool = true;    // Boolean
+let und;            // Undefined
+let n = null;       // Null
+let sym = Symbol("id"); // Symbol
+
+console.log(typeof str, typeof num, typeof big, typeof bool, typeof und, typeof n, typeof sym);
---
+++ Basics/variables.js
@@ -0,0 +1,16 @@
+var name="Alice";
+console.log(name);
+
+var name="Bob";
+console.log(name);
+
+let age=40;
+console.log(age);
+
+//let age=34; //variables with let datatype cannot be redeclared
+
+age=34;
+console.log(age);
+
+let dep;
+console.log(dep);
